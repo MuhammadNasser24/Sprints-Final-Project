08f304fbb13c7122f608f369a2609c38dc8af34d
Elsbelgy <121057828+MuhammadNasser24@users.noreply.github.com>
2023-08-26 03:50:36 +0300

    Add files via upload

---
+++ Docker/MySQL_Queries/BucketList.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE BucketList;
+
+USE BucketList;
+
+-- Contents of tbl_wish.sql
+CREATE TABLE `tbl_wish` (
+  `wish_id` int(11) NOT NULL AUTO_INCREMENT,
+  `wish_title` varchar(45) DEFAULT NULL,
+  `wish_description` varchar(5000) DEFAULT NULL,
+  `wish_user_id` int(11) DEFAULT NULL,
+  `wish_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`wish_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
